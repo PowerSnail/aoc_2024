38cfbf455c83ad1a9b68e2dd9224721cd7f7c865
PowerSnail <hj@powersnail.com>
2024-12-05 17:16:14 -0800

    day1 part 2

--- src/day1.rs
+++ src/day1.rs
@@ -1,6 +1,9 @@
+use std::{collections::HashMap, u64};
+
 use aoc_2024;
 
 fn main() {
+    let part = aoc_2024::get_part();
     let input = aoc_2024::get_stdin();
     let mut a1 = vec![];
     let mut a2 = vec![];
@@ -10,11 +13,24 @@ fn main() {
         a2.push(numbers[1].parse::<u64>().unwrap());
     }
 
-    a1.sort();
-    a2.sort();
-    let mut sum: u64 = 0;
-    for i in 0..a1.len() {
-        sum += a1[i].abs_diff(a2[i]);
+    if part == 1 {
+        a1.sort();
+        a2.sort();
+        let mut sum: u64 = 0;
+        for i in 0..a1.len() {
+            sum += a1[i].abs_diff(a2[i]);
+        }
+        println!("{}", sum);
+    } else {
+        let mut counts = HashMap::new();
+        for n in a2 {
+            let c = counts.get(&n).unwrap_or(&0);
+            counts.insert(n, c + 1);
+        }
+        let mut sum: u64 = 0;
+        for n in a1 {
+            sum += n * counts.get(&n).unwrap_or(&0);
+        }
+        println!("{}", sum);
     }
-    println!("{}", sum);
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,15 @@
-use std::io::{self, Read};
+use std::{
+    env,
+    io::{self, Read},
+};
 
 pub fn get_stdin() -> String {
     let mut string = String::new();
     let _ = io::stdin().read_to_string(&mut string);
     return string.trim().to_string();
+}
+
+pub fn get_part() -> u8 {
+    let args: Vec<_> = env::args().collect();
+    args[1].parse::<u8>().unwrap()
 }
